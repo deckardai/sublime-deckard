74e52ee35f5aa5f75b4eddb44a8464712ffd68b9
au <au@nau.re>
2017-01-19 08:31:17 +0100

    First version: send selection and save events

---
+++ deckard.py
@@ -0,0 +1,79 @@
+import sublime
+import sublime_plugin
+
+
+DdHost = "localhost:3325"
+
+import sys
+import os
+import json
+from time import time
+
+try:
+    if sys.version_info[0] >= 3:
+        import http.client as httplib
+    else:
+        import httplib
+
+    # Detect vim flavor
+    DdEditor = "sublime"
+    # Get a unique name
+    #DdEditor += ":" + str(int(time()))
+
+    DdOk = True
+except Exception as e:
+    # httplib can fail mysteriously in unusual environments, ex. git commit
+    DdOk = False
+
+
+
+class Deckard(sublime_plugin.EventListener):
+
+	def post(self, eventName, event):
+	    " Fire and forget an event "
+	    conn = httplib.HTTPConnection(DdHost, timeout=1)
+	    conn.request(
+	        "POST", "/" + eventName,
+	        body=json.dumps(event),
+	        headers={
+	            "Content-Type": "application/json",
+	        },
+	    )
+	    conn.close()
+
+	def on_modified_async(self, view):
+	    try:
+	        path = view.file_name()
+	        if not path:
+                    return
+	        self.post("change", {
+	            "fullPath": path,
+	        })
+	    except Exception as e:
+	        # Normal if Deckard is not running
+	        print(e)
+	        pass
+
+	def on_selection_modified_async(self, view):
+	    try:
+	        path = view.file_name()
+	        if not path:
+	            return
+
+	        sel = view.sel()[0]
+	        if sel.a == sel.b:
+	            return
+
+	        lineno, charno = view.rowcol(sel.a)
+
+	        event = {
+	            "path": path,
+	            "lineno": lineno,
+	            "charno": charno,
+	            "editor": DdEditor,
+	        }
+	        self.post("event", event)
+	    except Exception as e:
+	        # Normal if Deckard is not running
+	        print(e)
+	        pass
